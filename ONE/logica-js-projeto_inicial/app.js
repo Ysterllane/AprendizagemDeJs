56bcc1586c2cadc66277e6c8554f04f96fa72c9e
Ysterllane <ynrl@discente.ifpe.edu.br>
2024-01-17 16:21:41 -0300

    A

---
+++ ONE/logica-js-projeto_inicial/app.js
@@ -0,0 +1,37 @@
+
+/*alert('Boas-vindas ao jogo do número secreto!');
+let numeroSecreto = 5 ;
+let chute = prompt ('Escolha um número entre 1 e 10: ');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
+
+
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha == senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}*/
+
+alert ('Boas vindas ao nosso site!');
+
+//let nome = 'lua';
+let idede = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert ('Erro! Preencha todos os campos.');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos.';
+alert (mensagemDeErro);
+
+let nome = prompt ('Qual o nome do usuário? ');
+
+let idade = prompt ('Qual sua idede? ');
+
+if (idade >= 18){
+    alert ('Pode tirar a habilitação');
+}
