67d64a5b72e6cec4292bb161a86410d2e51db134
Ysterllane <ynrl@discente.ifpe.edu.br>
2024-01-21 13:54:28 -0300

    upppppp

--- ONE/js-curso-2-aula1/app.js
+++ ONE/js-curso-2-aula1/app.js
@@ -5,23 +5,62 @@ let paragrafafo = document.querySelector('p');
 paragrafafo.innerHTML = 'Escolha um número entre 1 e 100';*/
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 //Funçao com parâmetro
-function exibirTextoNaTela(tag, texto){
+function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto' );
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+exibirMensagemInicial();
 
 //Funçao sem retorno e sem parâmetro
 function verificarChute() {
     let chute = document.querySelector('input').value; // tipo boleano t/f
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = ('Você descobriu o número secreto com ' + tentativas + ' ' + palavraTentativa + '!');
+
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
+        }
+
+        else {
+            exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
+        }
+
+        tentativas++;
+        limparcampo();
+    }
 }
 
 //Funçao com retorno
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparcampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparcampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    ocument.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
 }
